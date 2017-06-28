105132b42273ecfc22938bca9d26333d736dcbad
Jerry Lin <jerrylin@AnnydeMacBook-Air.local>
2017-06-28 23:04:01 +0800

    some comments

--- play_game.py
+++ play_game.py
@@ -181,3 +181,49 @@ if __name__ == '__main__':
     # If dealer has an ace, 10, or face card, the dealer checks for blackjack. If the dealer has blackjack, 
     # any insurance bets are paid, and all other bets are settled. If the dealer does not have blackjack,
     # any insuraces bets are collected, player who have blackjack are paid 3:2 and the game continues.
+
+    # Each player whose bet has not yet been settled gets their turn.
+    # 1) if late surrender is allowed, they may surrender at any point provided they are not bust.
+    # 2) if they have two equal cards, they may split if allowed.
+    # 3) if they do not split, they may double down if allowed.
+    # 4) if they do not double down, they may hit as many times as they wish until they bust (go over 21)
+    #    or stand.
+
+    # If there are any players whose bet is not yet settled, the dealer's hole card is shown, the dealer hits 
+    # or stands as prescribed by the rules, and all remaining bets are collected or paid.
+
+
+    # Insurance
+    # When the dealer's face-up card is an ace, each player gets the chance to bet on whether the dealer has
+    # a blackjack or not. This is done before any other player actions.
+    # The insurance wager equals your original bet and is used to cancel out the likely loss of this bet.
+    # A winning insurance bet will be paid at odds of 2:1, and since you lose your original bet, you'll break
+    # even on the hand. Strategy guides tend to advice against taking insurance.
+
+    # Surrender
+    # If you have a bad hand compared to the dealer's hand (judging from what you can see of it), you can give
+    # up the hand and reclaim half of your bet. The casino keeps the other half uncontested. You need a really
+    # bad hand match-up for a surrender to be profitable, such as 16 vs the dealer showing a 10.
+
+    # Splitting
+    # When you get two starting cards of the same face value, you have the option to split the cards in two.
+    # You place another bet of the same size as the original bet and play on with two hands. (Note that it is
+    # legel to split 10-point cards even if they do not form a pair - for example you could split a jack and a
+    # queen.)
+    # When you've decided to split a hand, the dealer immediately deals a second card to each hand. Now, if you
+    # are dealt yet another pair, some casino allow you to split the hand again, while others don't.
+    # When you're done splitting, each of your hands will be treated separately, meaning that you will take cards
+    # to your first hand until you stand or bust, and then carry on with the next hand.
+    # If you split aces, you are dealt a second card to each hand as usual, but you are not allowed to take any 
+    # further cards (unless you are dealt another ace and split again). All hands resulting from splitting aces
+    # remain as two-card hands.
+    # If the second card dealt to a split ace is a 10-point card. You do not receive the blackjack bonus for this
+    # hand. It does however win against an ordinary 21 made of more than two cards. If the dealer also has a 
+    # blackjack, the result for this hand is a push as usual. In many places the same rule (no blackjack bonus)
+    # is played if an ace is dealt as the second card to a 10-point card after splitting.
+
+    # Doubling down
+    # If you're fairly sure that your hand will beat the dealer's, you can double your original bet. You're 
+    # sometimes allowed to double down for any amount up to the original bet amount. In most casinos you may
+    # double down on any hand, but some casinos require an opening hand worth 9, 10, or 11.
+    # When you've chosen to double down, you'll only get one more card from the dealer.
