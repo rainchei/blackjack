41fbe19f93b27cbb7c4f86b700690f4c83ab71eb
Jerry Lin <jerrylin@AnnydeMacBook-Air.local>
2017-06-28 23:01:50 +0800

    add guidance in readme

--- play_game.py
+++ play_game.py
@@ -164,86 +164,20 @@ def count_points(ranks):
 
     return points
 
+
 if __name__ == '__main__':
 
     all_players = player_sign_up()
-
-    # (0) The standard 52-card pack is used, but in most casinos several decks of cards are shuffled together. The
-    # six-deck game (312 cards) is the most popular. In addition, the dealer uses a blank plastic card, which is never
-    # dealt, but is placed toward the bottom of the pack to indicate when it will be time for cards to be reshuffled.
-    # When four or more decks are used, they are dealt from a shoe (a box that allows the dealer to remove card one at
-    # a time, face down, without actually holding one or more packs).
-
-    # (1) The dealer thoroughly shuffles portions of the pack until all the cards have been mixed and combined. He
-    # designated one of the players to cut, and the plastic insert card is placed so that the last 60 to 75 cards or so
-    # will not be used. (Not dealing to the bottom of all the cards makes it more difficult for professional counter to
-    # operate efficiently.)
-
     shoe_box = build_deck()
-
-    # (2) Each player places a bet, in front of him in the designated area, general limits are from $2 to $500.
-
     place_bet(all_players)
-
-    # (3) The dealer gives one card face up to each player in rotation clockwise, and then one card face up to himself.
-    # Another round of cards is then dealt face up to each player, but the dealer takes his second card face down.
-
+    # Each player is dealt two cards face up, then dealer receives one upcard and one hole card.
     deal_cards(all_players, shoe_box)
     print_table(all_players)
+    # If the dealer has an ace showing, the insurance bet is offered to each player in turn.
 
-    # (4) If any player has a natural and the dealer does not, the dealer immediately pays that player one and a half
-    # times the amount of his bet.
-    # If the dealer has a natural, he immediately collects the bets of all players who do not have naturals, (but no
-    # additional amount).
-    # If the dealer and another player both have naturals, the bet of that player is a stand-off (a tie), and the player
-    # takes back his chips.
-    # If the dealer's face-up card is a ten-card or an ace, he looks at his face-down card to see if the two cards make
-    # a natural. If the face-up card is not a ten-card or an ace, he does not look at the face-down card until it is
-    # the dealer's turn to play.
+    # If early surrander is allowed, each player has the option to surrender, taking back half his bet 
+    # and forfeiting the rest.
 
-    # (5) The player to the left goes first and must decide whether to "stand" (not ask for another card) or "hit"
-    # (ask for another card in an attempt to get closer to a count of 21, or even hit 21 exactly). When asking for
-    # additional cards, one at a time, he may go "bust" (if it is over 21). Then, the player loses and the dealer
-    # collects the bet wagered.
-
-    # (6) The combination of an ace with a card other than a ten-card is known as a "soft hand", because the player can
-    # count the ace as a 1 or 11, and either draw cards or not. For example, if the draw creates a bust hand by counting
-    # the aces as an 11, the player simply counts the aces as 1 and continues playing by standing or hitting.
-
-    # (7) When the dealer has served every player, his face-down card is turned up. If the total is 17 or more, he must
-    # stand. If the total is 16 or under, he must take a card. He must continue to take cards until the total is 17 or
-    # more, at which point the dealer must stand. If the dealer has an ace, and counting it as 11 would bring his total
-    # to 17 or more (but not over 21), he must count the ace as 11 and stand. The dealer's decisions, then, are
-    # automatic on all plays, whereas the player always has the option of taking one or more cards.
-
-    # (8) If a players's first two cards are of the same denomination, such as two jacks or two sixes, he may choose to
-    # treat them as two separate hands when his turn comes around. The amount of his original bet then goes on one of
-    # cards, and an equal amount must be placed as a bet on the other card. The player first plays the hand to his left
-    # by standing or hitting one or more times; only then is the hand to the right played. The two hands are thus
-    # treated separately, and the dealer settles with each on its own merits. With a pair of aces, the player is given
-    # one card for each ace and may not draw again. Also, if a ten-card is dealt to one of these aces, the payoff is
-    # equal to the bet (not one and one-half to one, as with a blackjack at any other time).
-
-    # (9) Another option open to the player is doubling his bet when the original two cards dealt total 9, 10, or 11.
-    # When the player's turn comes, he places a bet equal to the original bet, and the dealer gives him just one card,
-    # which is placed face down and is not turned up until the bets are settled at the end of the hand. With two fives,
-    # the player may split a pair, double down, or just play the hand in the regular way. Note that the dealer does not
-    # have the option of splitting or doubling down.
-
-    # (10) When the dealer's face-up card is an ace, any of the players may make a side bet of up to half the original
-    # bet that the dealer's face-down card is a ten-card, and thus a blackjack for the house.
-    # Once all such side bets are placed, the dealer looks at his hole card. If it is a ten-card, it is turned up, and
-    # those players who have made the insurance bet win and are paid double the amount of their half-bet
-    # - a 2 to 1 payoff.
-
-    # (11) A bet once paid and collected is never returned. Thus, one key advantage to the dealer is that the player
-    # goes first. If the player goes bust, he has already lost his wager, even if the dealer goes bust as well. If the
-    # dealer goes over 21, he pays each player who has stood the amount of that player's bet. If the dealer stands at 21
-    # or less, he pays the bet of any player having a higher total (not exceeding 21) and collects the bet of any player
-    # having a lower total. If there's a stand-off (a player having the same total as the dealer), no chips are paid out
-    # or collected.
-
-    # (12) When each player's bet is settled, the dealer gathers in that player's card and places them face up at the
-    # side against a clear plastic L-shaped shield. The dealer continues to deal from the shoe until he comes to the
-    # plastic insert card, which indicates that it is time to reshuffle. Once that round of play is over, the dealer
-    # reshuffles all the cards, prepares them for the cut, places the cards in the shoe, and the game continues.
+    # If dealer has an ace, 10, or face card, the dealer checks for blackjack. If the dealer has blackjack, 
+    # any insurance bets are paid, and all other bets are settled. If the dealer does not have blackjack,
+    # any insuraces bets are collected, player who have blackjack are paid 3:2 and the game continues.
